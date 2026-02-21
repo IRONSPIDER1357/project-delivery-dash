0654e9dab65860b88b852e82092cd604dc484a94
Nirmal Chaudhari <nirmalchaudhari1357@gmail.com>
2026-02-22 01:37:28 +0530

    feat: added a trigger detection and cinemachine to our car

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -2,8 +2,13 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other) 
+    private void OnCollisionEnter2D(Collision2D collision) 
     {
             Debug.Log("You crushed");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("You got a boost");
     }
 }
