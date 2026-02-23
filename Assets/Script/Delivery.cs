1cc1d90770a5585d295d0a74212a8f299da3e94b
Nirmal Chaudhari <nirmalchaudhari1357@gmail.com>
2026-02-24 00:57:24 +0530

    feat: added packet detection and delivered detection

--- Assets/Script/Delivery.cs
+++ Assets/Script/Delivery.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPacket;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Packet"))
+        if(collision.CompareTag("Packet") && hasPacket == false)
         {
             Debug.Log("Packet picked up!");
+            hasPacket = true;
         }
 
-        if(collision.CompareTag("Customer"))
+        if(collision.CompareTag("Customer") && hasPacket)
         {
             Debug.Log("Packet Delivered");
+            hasPacket = false;
         }
     }
 }
