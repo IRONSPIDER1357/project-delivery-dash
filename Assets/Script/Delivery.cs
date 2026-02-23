31ea5a65365038c81b1aea5d9e7425ecbac1bd72
Nirmal Chaudhari <nirmalchaudhari1357@gmail.com>
2026-02-24 01:26:08 +0530

    feat: added packet destroy function on pickup and particle system

--- Assets/Script/Delivery.cs
+++ Assets/Script/Delivery.cs
@@ -3,17 +3,21 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPacket;
+    [SerializeField] float destroyDelay = 1f; 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Packet") && hasPacket == false)
+        if(collision.CompareTag("Packet") && !hasPacket)
         {
             Debug.Log("Packet picked up!");
             hasPacket = true;
+            GetComponent<ParticleSystem>().Play();
+            Destroy(collision.gameObject, destroyDelay);
         }
 
         if(collision.CompareTag("Customer") && hasPacket)
         {
             Debug.Log("Packet Delivered");
+            GetComponent<ParticleSystem>().Stop();
             hasPacket = false;
         }
     }
